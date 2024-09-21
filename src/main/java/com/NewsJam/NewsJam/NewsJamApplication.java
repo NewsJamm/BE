05b3bfeb1dc1d406a4906fecd6683f6ef2cbee05
= <=>
2024-09-21 19:23:49 +0900

    🎉 Setup

---
+++ src/main/java/com/NewsJam/NewsJam/NewsJamApplication.java
@@ -0,0 +1,13 @@
+package com.NewsJam.NewsJam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsJamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewsJamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/NewsJam/NewsJam/NewsJamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.NewsJam.NewsJam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsJamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
