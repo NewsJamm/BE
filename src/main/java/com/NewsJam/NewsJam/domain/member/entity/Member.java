e23ec9993a3284bb4b2ea4b63d4cad9299480799
= <=>
2024-10-02 02:34:00 +0900

    :sparkles: 회원 속성에 권한 추가

--- src/main/java/com/NewsJam/NewsJam/domain/member/entity/Member.java
+++ src/main/java/com/NewsJam/NewsJam/domain/member/entity/Member.java
@@ -6,6 +6,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.Collection;
 import java.util.List;
 
 @Entity
@@ -30,6 +31,9 @@ public class Member {
     @Column(name = "scrap_list")
     @OneToMany(mappedBy = "memberId", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Scrap> scrapList;
+
+    @OneToMany(mappedBy = "member", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Authority> authorities;
 
     public void addScrap(Scrap scrap) {
         scrapList.add(scrap);
