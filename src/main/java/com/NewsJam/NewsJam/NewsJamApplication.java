e01100d18d3446a3c3b8c96ce2c768050a0d799b
= <=>
2024-10-30 01:42:35 +0900

    :art: 스프링 스케줄러 사용을 위한 어노테이션 추가

--- src/main/java/com/NewsJam/NewsJam/NewsJamApplication.java
+++ src/main/java/com/NewsJam/NewsJam/NewsJamApplication.java
@@ -5,11 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableJpaRepositories
 @EnableCaching
 @EnableJpaAuditing
+@EnableScheduling
 public class NewsJamApplication {
 
 	public static void main(String[] args) {
