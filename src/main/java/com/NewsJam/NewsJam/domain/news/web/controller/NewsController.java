fbe00d605803a6d2505893b85df34b3c6e96396e
= <=>
2024-10-30 01:44:28 +0900

    :sparkles: NewsAPI 호출을 통한 뉴스 크롤링 컨트롤러 작성

---
+++ src/main/java/com/NewsJam/NewsJam/domain/news/web/controller/NewsController.java
@@ -0,0 +1,28 @@
+package com.NewsJam.NewsJam.domain.news.web.controller;
+
+import com.NewsJam.NewsJam.domain.news.scheduler.SchedulerConfig;
+import com.NewsJam.NewsJam.domain.news.web.dto.NewsAPIRequestDto;
+import com.NewsJam.NewsJam.domain.news.web.dto.NewsAPIResponseDto;
+import com.NewsJam.NewsJam.global.response.ApiResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@Slf4j
+@RequiredArgsConstructor
+public class NewsController {
+    private final SchedulerConfig schedulerConfig;
+
+    @GetMapping("/getNews")
+    public ApiResponse<List<NewsAPIResponseDto.NewsData>> getNewsToAIServer(@Validated NewsAPIRequestDto.Keywords keywords){
+        return ApiResponse.onSuccess(schedulerConfig.scheduled(keywords));
+    }
+
+
+}
