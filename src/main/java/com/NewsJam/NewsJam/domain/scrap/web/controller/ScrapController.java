f0f5db4dcc3317f03cbb9c39a10627bed6276e65
= <=>
2024-10-31 01:25:32 +0900

    :sparkles: Scrap Controller 작성

---
+++ src/main/java/com/NewsJam/NewsJam/domain/scrap/web/controller/ScrapController.java
@@ -0,0 +1,41 @@
+package com.NewsJam.NewsJam.domain.scrap.web.controller;
+
+import com.NewsJam.NewsJam.domain.member.entity.Member;
+import com.NewsJam.NewsJam.domain.scrap.Service.ScrapService;
+import com.NewsJam.NewsJam.domain.scrap.web.dto.ScrapRequestDto;
+import com.NewsJam.NewsJam.domain.scrap.web.dto.ScrapResponseDto;
+import com.NewsJam.NewsJam.domain.scrap.web.dto.ScrapUndoRequestDto;
+import com.NewsJam.NewsJam.domain.scrap.web.dto.ScrapUndoResponseDto;
+import com.NewsJam.NewsJam.global.response.ApiResponse;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@Slf4j
+public class ScrapController {
+    private final ScrapService scrapService;
+
+    @PostMapping("/scrap")
+    public ApiResponse<ScrapResponseDto.ScrapResponse> scrap(@Valid @RequestBody ScrapRequestDto.ScrapRequest scrapRequestDto, Member member) {
+        log.info("scrap request: {}", scrapRequestDto);
+        log.info("member: {}", member);
+
+        ScrapResponseDto.ScrapResponse response = scrapService.scrap(scrapRequestDto, member.getId());
+        return ApiResponse.onSuccess(response);
+    }
+
+    @DeleteMapping("/scrapUndo")
+    public ApiResponse<ScrapUndoResponseDto.ScrapUndoResponse> scrapUndo(@Valid @RequestBody ScrapUndoRequestDto.ScrapUndoRequest undoRequestDto, Member member) {
+        log.info("undo request: {}", undoRequestDto);
+        log.info("member: {}", member);
+
+        ScrapUndoResponseDto.ScrapUndoResponse response = scrapService.scrapUndo(undoRequestDto, member.getId());
+        return ApiResponse.onSuccess(response);
+    }
+}
