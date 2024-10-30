9d7e825b69520f6930f359a10034517d7825d856
= <=>
2024-10-31 01:26:43 +0900

    :sparkles: ScrapRepository 작성

---
+++ src/main/java/com/NewsJam/NewsJam/domain/scrap/repository/ScrapRepository.java
@@ -0,0 +1,7 @@
+package com.NewsJam.NewsJam.domain.scrap.repository;
+
+import com.NewsJam.NewsJam.domain.scrap.entity.Scrap;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ScrapRepository extends JpaRepository<Scrap, Long> {
+}
