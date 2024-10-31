28e31e54370ec3a7bcc5844d9341af5c752dac83
junhaa <2171326@hansung.ac.kr>
2024-10-31 18:00:09 +0900

    :sparkles: FEAT. SecurityConfig 로그인 관련 필터 적용
    - AuthenticationManager 구현 및 필터 적용

--- src/main/java/com/NewsJam/NewsJam/global/security/config/SecurityConfig.java
+++ src/main/java/com/NewsJam/NewsJam/global/security/config/SecurityConfig.java
@@ -1,12 +1,28 @@
 package com.NewsJam.NewsJam.global.security.config;
 
-import com.NewsJam.NewsJam.global.security.jwt.filter.JwtTokenFilter;
-import com.NewsJam.NewsJam.global.security.jwt.filter.JwtTokenProvider;
-import com.NewsJam.NewsJam.global.security.service.CustomUserDetailService;
-import com.NewsJam.NewsJam.global.security.service.CustomOAuth2UserService;
+import com.NewsJam.NewsJam.domain.member.repository.AuthorityRepository;
+import com.NewsJam.NewsJam.domain.member.repository.MemberRepository;
+import com.NewsJam.NewsJam.domain.member.service.MemberQueryService;
+import com.NewsJam.NewsJam.global.security.jwt.authentication.SocialAuthenticationProvider;
+import com.NewsJam.NewsJam.global.security.jwt.filter.SocialLoginAuthenticationFilter;
+import com.NewsJam.NewsJam.global.security.jwt.filter.LocalLoginAuthenticationFilter;
+import com.NewsJam.NewsJam.global.security.jwt.filter.JwtAuthenticationFilter;
+import com.NewsJam.NewsJam.global.security.jwt.handler.JwtLoginFailureHandler;
+import com.NewsJam.NewsJam.global.security.jwt.handler.JwtLoginSuccessHandler;
+import com.NewsJam.NewsJam.global.security.jwt.handler.OAuthLoginFailureHandler;
+import com.NewsJam.NewsJam.global.security.jwt.handler.OAuthLoginSuccessHandler;
+import com.NewsJam.NewsJam.global.security.jwt.service.JwtService;
+import com.NewsJam.NewsJam.global.security.service.LocalLoginUserDetailService;
+import com.NewsJam.NewsJam.global.security.service.LoginUserDetailsService;
+import com.NewsJam.NewsJam.global.service.SocialLoginService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.Arrays;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -14,16 +30,25 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.logout.LogoutFilter;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
 
-    private final CustomUserDetailService customUserDetailService;
-    private final CustomOAuth2UserService customOAuth2UserService;
-    private final JwtTokenProvider jwtTokenProvider;
+    private final LocalLoginUserDetailService localLoginUserDetailService;
+    private final MemberRepository memberRepository;
+    private final LoginUserDetailsService loginUserDetailsService;
+    private final JwtService jwtService;
+    private final MemberQueryService memberQueryService;
+    private final AuthorityRepository authorityRepository;
+    private final ObjectMapper objectMapper;
+    private final JwtLoginFailureHandler jwtLoginFailureHandler;
+    private final JwtLoginSuccessHandler jwtLoginSuccessHandler;
+    private final OAuthLoginFailureHandler oAuthLoginFailureHandler;
+    private final OAuthLoginSuccessHandler oAuthLoginSuccessHandler;
+    private final SocialLoginService socialLoginService;
 
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -42,22 +67,51 @@ public class SecurityConfig {
                         .requestMatchers("/", "/login","/signup").permitAll()
                         .requestMatchers("/api/**").authenticated()
                         .anyRequest().authenticated()
-                )
+                );
 
-                // OAuth2
-                .oauth2Login(oauth2Login -> oauth2Login
-                        .loginPage("/login")
-                        .defaultSuccessUrl("/home", true)
-                        .userInfoEndpoint(userInfo ->
-                                userInfo.userService(customOAuth2UserService) // OAuth2로 받아온 사용자 정보를 처리
-                        )
-                )
-
-                // jwt
-                .addFilterBefore(new JwtTokenFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class)
-                .userDetailsService(customUserDetailService);
-
+        http.addFilterAfter(socialLoginAuthenticationFilter(), LogoutFilter.class);
+        http.addFilterAfter(localLoginAuthenticationFilter(), SocialLoginAuthenticationFilter.class);
+        http.addFilterBefore(jwtAuthenticationProcessingFilter(), SocialLoginAuthenticationFilter.class);
 
         return http.build();
     }
+
+    @Bean
+    public AuthenticationManager authenticationManager() {
+        DaoAuthenticationProvider localLoginProvider = new DaoAuthenticationProvider();
+        localLoginProvider.setPasswordEncoder(passwordEncoder());
+        localLoginProvider.setUserDetailsService(loginUserDetailsService);
+
+        SocialAuthenticationProvider socialLoginProvider = new SocialAuthenticationProvider(socialLoginService,
+                loginUserDetailsService);
+        return new ProviderManager(Arrays.asList(localLoginProvider, socialLoginProvider));
+    }
+
+    @Bean
+    public LocalLoginAuthenticationFilter localLoginAuthenticationFilter() {
+        LocalLoginAuthenticationFilter customJsonUsernamePasswordLoginFilter
+                = new LocalLoginAuthenticationFilter(objectMapper);
+        customJsonUsernamePasswordLoginFilter.setAuthenticationManager(authenticationManager());
+        customJsonUsernamePasswordLoginFilter.setAuthenticationSuccessHandler(jwtLoginSuccessHandler);
+        customJsonUsernamePasswordLoginFilter.setAuthenticationFailureHandler(jwtLoginFailureHandler);
+        return customJsonUsernamePasswordLoginFilter;
+    }
+
+    @Bean
+    SocialLoginAuthenticationFilter socialLoginAuthenticationFilter(){
+        SocialLoginAuthenticationFilter socialLoginAuthenticationFilter
+                = new SocialLoginAuthenticationFilter(objectMapper);
+        socialLoginAuthenticationFilter.setAuthenticationManager(authenticationManager());
+        socialLoginAuthenticationFilter.setAuthenticationSuccessHandler(oAuthLoginSuccessHandler);
+        socialLoginAuthenticationFilter.setAuthenticationFailureHandler(oAuthLoginFailureHandler);
+        return socialLoginAuthenticationFilter;
+    }
+
+    @Bean
+    public JwtAuthenticationFilter jwtAuthenticationProcessingFilter() {
+        JwtAuthenticationFilter jwtAuthenticationFilter = new JwtAuthenticationFilter(jwtService,
+                memberRepository, memberQueryService, authorityRepository);
+        return jwtAuthenticationFilter;
+    }
+
 }
