91ed2f714a61b84aa0fc1535990d6fb2a635c7cf
= <=>
2024-10-31 18:38:58 +0900

    :art: Exception 코드 매개변수 수정

--- src/main/java/com/NewsJam/NewsJam/domain/member/exception/ExistLoginIdException.java
+++ src/main/java/com/NewsJam/NewsJam/domain/member/exception/ExistLoginIdException.java
@@ -1,10 +1,10 @@
 package com.NewsJam.NewsJam.domain.member.exception;
 
-import com.NewsJam.NewsJam.global.enums.statuscode.ErrorStatus;
+import com.NewsJam.NewsJam.global.enums.statuscode.BaseCode;
 import com.NewsJam.NewsJam.global.exception.GeneralException;
 
 public class ExistLoginIdException extends GeneralException {
-    public ExistLoginIdException() {
-        super(ErrorStatus._EXIST_LOGINID);
+    public ExistLoginIdException(BaseCode errorStatus) {
+        super(errorStatus);
     }
 }
