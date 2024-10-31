6ac52c30b6f5986ef2ced4769a13ffe32140ed84
= <=>
2024-11-01 00:28:37 +0900

    :art: permit all (임시)

--- src/main/java/com/NewsJam/NewsJam/global/security/config/SecurityConfig.java
+++ src/main/java/com/NewsJam/NewsJam/global/security/config/SecurityConfig.java
@@ -62,7 +62,7 @@ public class SecurityConfig {
                 .formLogin(AbstractHttpConfigurer::disable)
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/", "/login","/signup").permitAll()
+                        .requestMatchers("/**").permitAll()
                         .requestMatchers("/api/**").authenticated()
                         .anyRequest().authenticated()
                 );
