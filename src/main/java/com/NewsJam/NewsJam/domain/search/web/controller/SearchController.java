5d511a8663b140147f56a27b995db67ebfcddfe6
= <=>
2024-10-31 19:45:46 +0900

    :sparkles: 검색 기능을 위한 SearchController 작성

---
+++ src/main/java/com/NewsJam/NewsJam/domain/search/web/controller/SearchController.java
@@ -0,0 +1,24 @@
+package com.NewsJam.NewsJam.domain.search.web.controller;
+
+import com.NewsJam.NewsJam.domain.search.service.SearchService;
+import com.NewsJam.NewsJam.domain.search.web.dto.SearchResultResponseDto;
+import com.NewsJam.NewsJam.global.response.ApiResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@Slf4j
+public class SearchController {
+    private final SearchService searchService;
+
+    @GetMapping("/search")
+    public ApiResponse<SearchResultResponseDto.SearchResult> search(@RequestParam String query) {
+        log.info("Search query: {}", query);
+        SearchResultResponseDto.SearchResult response = searchService.search(query);
+        return ApiResponse.onSuccess(response);
+    }
+}
