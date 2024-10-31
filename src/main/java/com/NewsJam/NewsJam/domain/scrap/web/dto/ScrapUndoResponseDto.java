8f7b914ac1717bf45983efc7fb27de9d477afecc
= <=>
2024-11-01 00:28:23 +0900

    :art: add annotations

--- src/main/java/com/NewsJam/NewsJam/domain/scrap/web/dto/ScrapUndoResponseDto.java
+++ src/main/java/com/NewsJam/NewsJam/domain/scrap/web/dto/ScrapUndoResponseDto.java
@@ -1,10 +1,16 @@
 package com.NewsJam.NewsJam.domain.scrap.web.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 public class ScrapUndoResponseDto {
 
+    @Data
     @Builder
+    @AllArgsConstructor
+    @NoArgsConstructor
     public static class ScrapUndoResponse{
         Long scrapId;
         Long memberId;
