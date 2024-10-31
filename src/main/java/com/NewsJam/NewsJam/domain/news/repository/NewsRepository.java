5127fb2a2297b1b1b7353c90bf6a7cf2c6342b2f
= <=>
2024-10-31 19:45:33 +0900

    :sparkles: 검색 기능을 위한 JPA 메서드 추가

--- src/main/java/com/NewsJam/NewsJam/domain/news/repository/NewsRepository.java
+++ src/main/java/com/NewsJam/NewsJam/domain/news/repository/NewsRepository.java
@@ -1,6 +1,8 @@
 package com.NewsJam.NewsJam.domain.news.repository;
 
 import com.NewsJam.NewsJam.domain.news.entity.News;
+
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -8,5 +10,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface NewsRepository extends JpaRepository<News, Long> {
     Optional<News> findByOriginalLink(String originalLink);
-
+    Optional<List<News>> findByTitleContainingByPubDateDesc(String title);
 }
