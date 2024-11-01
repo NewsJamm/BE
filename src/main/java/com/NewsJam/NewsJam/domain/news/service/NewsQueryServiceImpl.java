626bdda6bba40b6f4c3f82b9f466d16fdde18edf
junhaa <2171326@hansung.ac.kr>
2024-11-01 17:07:13 +0900

    :sparkles: FEAT. 카테고리 별 뉴스 데이터 검색 서비스 로직 구현

--- src/main/java/com/NewsJam/NewsJam/domain/news/service/NewsQueryServiceImpl.java
+++ src/main/java/com/NewsJam/NewsJam/domain/news/service/NewsQueryServiceImpl.java
@@ -1,6 +1,7 @@
 package com.NewsJam.NewsJam.domain.news.service;
 
 import com.NewsJam.NewsJam.domain.news.entity.News;
+import com.NewsJam.NewsJam.domain.news.enums.NewsCategory;
 import com.NewsJam.NewsJam.domain.news.repository.NewsRepository;
 import com.NewsJam.NewsJam.global.paging.enums.SortStatus;
 import com.NewsJam.NewsJam.global.paging.exception.InvalidSortStatusException;
@@ -20,6 +21,7 @@ import org.springframework.transaction.annotation.Transactional;
 public class NewsQueryServiceImpl implements NewsQueryService {
     private final NewsRepository newsRepository;
 
+    @Override
     public Page<News> getHotTopicKeywordNewsPage(String keyword, SortStatus sortStatus, Integer page,
                                                  Integer pageSize) {
         Pageable pageable = toPageable(sortStatus, page, pageSize);
@@ -28,16 +30,25 @@ public class NewsQueryServiceImpl implements NewsQueryService {
         return result;
     }
 
+    @Override
+    public Page<News> getCategoryNewsPage(NewsCategory category, int page, int size,
+                                          SortStatus sortStatus) {
+        Pageable pageable = toPageable(sortStatus, page, size);
+        return newsRepository.findByNewsCategory(category, pageable);
+
+    }
+
     private Pageable toPageable(SortStatus sortStatus, Integer page, Integer pageSize) {
         Sort sort;
         if (sortStatus == SortStatus.LATEST) {
             sort = Sort.by(Sort.Direction.DESC, "createdAt");
-//        } else if (sortStatus == SortStatus.POPULAR) {
-//            sort = Sort.by(Sort.Direction.DESC, "viewCount");
+        } else if (sortStatus == SortStatus.POPULAR) {
+            sort = Sort.by(Sort.Direction.DESC, "viewCount");
         } else {
             throw new InvalidSortStatusException();
         }
         return PageRequest.of(page - 1, pageSize, sort);
     }
+
 
 }
