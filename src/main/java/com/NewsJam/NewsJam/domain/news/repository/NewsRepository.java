0d0a559545bc59e1c3f213b568ee3c82242bd2bc
= <=>
2024-11-01 12:44:33 +0900

    :sparkles: 카테고리 별 뉴스 띄우기를 위한 JPA 메서드 추가

--- src/main/java/com/NewsJam/NewsJam/domain/news/repository/NewsRepository.java
+++ src/main/java/com/NewsJam/NewsJam/domain/news/repository/NewsRepository.java
@@ -4,6 +4,10 @@ import com.NewsJam.NewsJam.domain.news.entity.News;
 
 import java.util.List;
 import java.util.Optional;
+
+import com.NewsJam.NewsJam.domain.news.enums.NewsCategory;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -13,4 +17,5 @@ public interface NewsRepository extends JpaRepository<News, Long> {
 
     Optional<List<News>> findByNewsTitleContainingOrderByPubDateDesc(String query);
 
+    Page<News> findByNewsCategory(NewsCategory category, Pageable pageable);
 }
